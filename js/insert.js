8466b695478c5acaee89c0e51e17fd8dda59edf2
吕雷 <958141982@qq.com>
2017-08-21 21:33:53 +0800

    Create insert.js

---
+++ js/insert.js
@@ -0,0 +1,183 @@
+$(function(){
+	var insert = {},
+		queryBtn,
+		meText,
+		inBtn,
+		robotText,
+		delBtn,
+		delLookBtn,
+		modLookBbtn,
+		texts,
+		flag=0,
+		modMeText,
+		modRobotText,
+		lastMeText,
+		modBtn;
+
+
+
+	insert = {
+		init:function(){
+			this.in();
+			this.query();
+			this.del();
+			this.mod();
+		},
+		show:function(data){
+			var box = $('<div class="tips">'+data+'</div>');
+				$('body').prepend(box);
+				$('.tips').show(1000);
+				setTimeout(function(){
+					$('.tips').animate({
+						top:'-40px'
+					},1000);
+				},1000)
+		},
+		in:function(){
+			inBtn = $('#in-btn');
+			inBtn.click(function(){
+				flag = 0;
+				meText = $('#meText').val();
+				robotText = $('#robotText').val();
+				if(meText==''||robotText==''){
+					alert('都要输入！');
+				}else{
+					$.ajax({
+						url:'./php/in.php',
+						type:'POST',
+						data:{
+							meText:meText,
+							robotText:robotText
+						},
+						success:function(data){
+							insert.show(data);
+						}
+					})
+				}
+				$('#meText').val('');
+				$('#robotText').val('');
+				insert.queryFn();
+			})
+		},
+		queryFn:function(){
+				if(flag==0){
+					$('#query-show table').html('');
+					$.ajax({
+						url:'./php/query.php',
+						type:'GET',
+						success:function(data){
+							var result = '<tr><td>id</td><td>me</td><td>robot</td></tr>';
+							result += data;
+							$('#query-show table').html(result);
+							insert.show('查询成功');
+						}
+					});
+					flag = 1;
+				}else{
+					$('#query-show table').html('');
+					flag = 0;
+				}
+		},
+		query:function(){
+			queryBtn = $('#query-btn');
+			queryBtn.click(insert.queryFn);
+		},
+		del:function(){
+			delLookBtn = $('#del-look-btn');
+			delLookBtn.click(function(){
+				texts = $('#del-text').val();
+				$.ajax({
+					url:'./php/look.php',
+					type:'POST',
+					data:{
+						texts:texts
+					},
+					success:function(data){
+						if(data==0){
+							data = '不存在此值';
+						}else{
+							var result = '<tr><td>id</td><td>me</td><td>robot</td></tr>';
+							result += data;
+							$('#del-show table').html(result);
+							data = '查询成功';
+						}
+						insert.show(data);
+					}
+				});
+			})
+			delBtn = $('#del-btn');
+			delBtn.click(function(){
+				delText = $('#del-text').val();
+				var delConfirm = confirm('确定删除？');
+				if(delConfirm){
+					$.ajax({
+						url:'./php/del.php',
+						type:'POST',
+						data:{
+							delText:delText
+						},
+						success:function(data){
+							insert.show(data);
+							}
+					});
+					flag = 0;
+					insert.queryFn();
+					$('#del-show table').html('数据已不存在');
+					$('#del-text').val('');
+					
+				}else{
+					return false;
+				}	
+			})
+		},
+		mod:function(){
+			modLookBbtn = $('#mod-look-btn');
+			modBtn = $('#mod-btn');
+			modLookBbtn.click(function(){
+				texts = $('#mod-look-text').val();
+				$.ajax({
+					url:'./php/look.php',
+					type:'POST',
+					data:{
+						texts:texts
+					},
+					success:function(data){
+						if(data==0){
+							data = '不存在此值';
+						}else{
+							var result = '<tr><td>id</td><td>me</td><td>robot</td></tr>';
+							result += data;
+							$('#mod-show table').html(result);
+							data = '查询成功';
+						}
+						insert.show(data);
+					}
+				});
+				$('#del-text').val('');
+			})
+
+			modBtn.click(function(){
+				lastMeText = $('#mod-look-text').val();
+				modMeText = $('#mod-me-text').val();
+				modRobotText = $('#mod-robot-text').val();
+				$.ajax({
+					url:'./php/mod.php',
+					type:'POST',
+					data:{
+						lastMeText:lastMeText,
+						modMeText:modMeText,
+						modRobotText:modRobotText
+					},
+					success:function(data){
+						insert.show(data);
+					}
+				})
+				$('#mod-me-text').val('');
+				$('#mod-robot-text').val('');
+				flag = 0;
+				insert.queryFn();
+			})
+		}
+	}
+	insert.init();
+})
